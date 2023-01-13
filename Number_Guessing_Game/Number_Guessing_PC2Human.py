7a34dce39c325a74487191af3af7773103d37ae4
alihaghayegh <haghayeghali95@gmail.com>
2023-01-13 12:17:15 +0330

    2nd: Number Guessing Game added

---
+++ Number_Guessing_Game/Number_Guessing_PC2Human.py
@@ -0,0 +1,16 @@
+import random
+
+# PC number choose
+Number = random.randint(1 , 99)
+# Human guess
+guess = int(input("What's your guess...??? "))
+
+# repeat process
+while guess != Number :
+    if guess > Number :
+       print ("try smaller !")
+    elif guess < Number :
+        print ("try bigger !")
+    guess = int(input("What's your guess...??? "))
+# win message
+print ("You did it...")
