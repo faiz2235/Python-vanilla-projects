81b78bdacf8d815ce26e6490b08c18b52233e4ed
alihaghayegh <haghayeghali95@gmail.com>
2023-01-27 13:17:19 +0330

    8th: speed test added

---
+++ Speed_Test/speed_test.py
@@ -0,0 +1,10 @@
+"""
+    Speed Test using python
+"""
+
+import subprocess
+
+returned_text = subprocess.check_output(
+    "speedtest-cli", shell=True, universal_newlines=True)
+print("The Result of Speed Test")
+print(returned_text)
